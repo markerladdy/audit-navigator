de387db9d73ea3a2e022024a44f44efd10477a1d
政会智审 <contact@auditnavigator.de5.net>
2026-06-23 08:57:59 +0800

    chore: 清理临时脚本文件

--- check_deploy4.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Decode then deploy
-TK=$(echo "Y2Z1dF9kQUZHS2hMNjY1NVJKYVBuTjR1U3o5V1d5aHNmN0NyUFo1amtGMDNQMDE3NDc1NDI=" | base64 -d)
-ACC="be25ba83fb3a7d2a721187c12f1532c5"
-
-curl -s "https://api.cloudflare.com/client/v4/accounts/${ACC}/pages/projects/audit-navigator/deployments?per_page=1" \
-  -H "Authorization: Bearer *** | head -c 2000
